2a7c728125facf26d95d56cbcffa4ad291904319
Jollu8 <zholu8@gmail.com>
2022-10-31 22:59:03 +0300

    changed all file

---
+++ cmake-build-release/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/jolluemil/Desktop/ipfilter2
+# Build directory: /Users/jolluemil/Desktop/ipfilter2/cmake-build-release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "unit_tests")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/jolluemil/Desktop/ipfilter2/CMakeLists.txt;92;add_test;/Users/jolluemil/Desktop/ipfilter2/CMakeLists.txt;0;")
